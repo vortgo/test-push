108bfa1a42f681bebb9a8bf353cf42afa72192ca
vortgo <vortgo@gmail.com>
2018-07-23 10:28:15 +0300

    Delete firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: "54723132095"
-});
-
-const messaging = firebase.messaging();
